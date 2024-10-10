985c93888b955adf9c143378cb3999ecb9dc974f
vineel31 <64918224+vineel31@users.noreply.github.com>
2024-10-10 19:17:41 -0400

    Fixed errors

--- task2.sh
+++ task2.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Task: List files containing the word "sample" and at least 3 occurrences of "CSC510"
-grep -rl "sample" dataset1/ | \
-xargs -I{} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "$count {}"; fi' | \
-
-# Sort in descending order by count, then by file size
-sort -k1,1nr -k2,2n | \
-
-gawk '{size=$(stat -c "%s" $2); print $1, size, $2}' | \
-sed 's/file_/filtered_/g; s/^.* //g' > output.txt
-
-# Output the results
-cat output.txt
+grep -rl 'sample' dataset1/ | xargs -I {} sh -c '
+    count=$(grep -o "CSC510" "{}" | wc -l);
+    if [ "$count" -ge 3 ]; then
+        echo "$count {}";
+    fi' | gawk '{print $1, $2}' | xargs -I {} sh -c '
+    size=$(stat --format="%s" "$(echo {} | cut -d" " -f2)");
+    echo "$(echo {} | cut -d" " -f1) $size $(echo {} | cut -d" " -f2)"' | sort -k1,1nr -k2,2nr | sed 's/dataset1\/\///g' | sed 's/file_/filtered_/'
