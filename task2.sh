0eca66d6aa7f983086b99c966f856ff2ede768ff
Chandrakant Koneti <80403832+konetichandrakant@users.noreply.github.com>
2024-10-10 09:38:50 -0400

    Updated task2.sh

--- task2.sh
+++ task2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-grep -rl "sample" dataset1/ | \
-xargs -I{} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "$count {}"; fi' | \
-sort -k1,1nr -k2,2n | \
-sed 's/file_/filtered_/g; s/^.* //g' > output.txt
-
-# output
-cat output.txt
+#task a
+grep -rl 'sample' dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "$count {}"; fi' | \
+#task b
+gawk '{print $1, $2}' | xargs -I {} sh -c 'size=$(stat -f "%z" "$(echo {} | cut -d" " -f2)"); echo "$(echo {} | cut -d" " -f1) $size $(echo {} | cut -d" " -f2)"' | sor    t -k1,1nr -k2,2nr | \
+#task c
+sed 's/dataset1\/\///g' | sed 's/file_/filtered_/'
